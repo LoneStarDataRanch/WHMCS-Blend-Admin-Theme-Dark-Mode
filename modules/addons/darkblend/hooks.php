94340900a3cb9aa79809f1e8f076a67284c8e72d
LoneStarDataRanch <sales@lonestardataranch.com>
2025-08-20 09:51:18 -0500

    Update hooks.php
    adding in dark/light toggle

--- modules/addons/darkblend/hooks.php
+++ modules/addons/darkblend/hooks.php
@@ -43,6 +43,7 @@ function admin_blend_stats_hook($vars) {
 	$showTime		= Capsule::table('tbladdonmodules')->where('module', 'darkblend')->where('setting', 'datetime_enable')->first();
 	$showTickets	= Capsule::table('tbladdonmodules')->where('module', 'darkblend')->where('setting', 'ticketcount_enable')->first();
 	$ticketsTotal 	= Capsule::select(Capsule::raw('SELECT COUNT(t1.id) AS total FROM tbltickets AS t1 LEFT JOIN tblticketstatuses AS t2 ON t1.status = t2.title WHERE t2.showawaiting = "1" AND merged_ticket_id = "0"'))[0]->total;	
+	$showToggle		= Capsule::table('tbladdonmodules')->where('module', 'darkblend')->where('setting', 'toggle_enable')->first();
 
 
 	if ($showTickets->value && $ticketsTotal > 0) {
@@ -60,12 +61,16 @@ HTML;
 		$time = '<li class="nav-time" title="' . date('M d Y, H:i') . '"><small><span class="v8navstats"><span class="icon-container"><i class="icon fas fa-clock"></i></span><span class="nav-date">' . date('M d, H:i') . '</span><span class="nav-clock"></span></span></small></li>';
 	}
 
+	if ($showToggle->value) {
+		$toggleButton = '<li class="nav-toggle" title="Toggle Dark/Light Mode"><small><span class="theme-toggle-container"><i class="theme-toggle-icon fas fa-moon" id="theme-toggle"></i></span></small></li>';
+	}
+
 	return <<<HTML
 <script type="text/javascript">
 
 	$(document).on('ready', function() {
 
-		$('ul.right-nav').first('li').prepend('{$awaitingTicketsJS}{$time}');
+		$('ul.right-nav').first('li').prepend('{$awaitingTicketsJS}{$time}{$toggleButton}');
 
 		$("*[id=\'v8navstats\']").on("click", function(e) {
 			e.preventDefault();
@@ -75,6 +80,51 @@ HTML;
 		$('#v8navstats').next('ul').css({"width": "340px", "left": "-134px"});
 
 		$('span.v8navstats').css({"font-weight": "700"});
+		
+		// Dark mode toggle functionality
+		function toggleDarkMode() {
+			const darkCSS = document.getElementById('darkblend-css');
+			const toggleIcon = document.getElementById('theme-toggle');
+			const isDark = localStorage.getItem('whmcs-dark-mode') === 'true';
+			
+			if (isDark) {
+				// Switch to light mode
+				if (darkCSS) darkCSS.disabled = true;
+				toggleIcon.className = 'theme-toggle-icon fas fa-sun';
+				localStorage.setItem('whmcs-dark-mode', 'false');
+			} else {
+				// Switch to dark mode
+				if (darkCSS) darkCSS.disabled = false;
+				toggleIcon.className = 'theme-toggle-icon fas fa-moon';
+				localStorage.setItem('whmcs-dark-mode', 'true');
+			}
+			
+			// Add rotation animation
+			toggleIcon.style.transform = 'rotate(360deg)';
+			setTimeout(() => {
+				toggleIcon.style.transform = 'rotate(0deg)';
+			}, 300);
+		}
+		
+		// Initialize theme on page load
+		const savedTheme = localStorage.getItem('whmcs-dark-mode');
+		const darkCSS = document.getElementById('darkblend-css');
+		const toggleIcon = document.getElementById('theme-toggle');
+		
+		if (savedTheme === 'false' && darkCSS) {
+			darkCSS.disabled = true;
+			if (toggleIcon) toggleIcon.className = 'theme-toggle-icon fas fa-sun';
+		} else {
+			if (darkCSS) darkCSS.disabled = false;
+			if (toggleIcon) toggleIcon.className = 'theme-toggle-icon fas fa-moon';
+			localStorage.setItem('whmcs-dark-mode', 'true');
+		}
+		
+		// Bind click event to toggle
+		$(document).on('click', '#theme-toggle', function(e) {
+			e.preventDefault();
+			toggleDarkMode();
+		});
 	});
 
 </script>
